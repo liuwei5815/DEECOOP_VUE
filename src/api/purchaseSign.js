c44dac59e43bac6fa3acb9519c0d1224ff389766
liuwei <463681506@qq.com>
2019-12-23 15:52:28 +0800

    删除api多余文件

--- src/api/purchaseSign.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function listPagePurchaseSigns(data) {
-  return request({
-    url: '/purchase/purchaseSign/listPagePurchaseSigns',
-    method: 'post',
-    data: data
-  })
-}
